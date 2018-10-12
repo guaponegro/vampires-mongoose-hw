285f90d9947ab9641c036492848556a3a900e542
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-11 19:00:57 -0600

    queried for vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -28,94 +28,145 @@ mongoose.connection.on("error", () => {
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-Vampire.collection.insertMany(vampireData,() => {
-    console.log(vampireData)
-    mongoose.connection.close();
-  });
+
+
+// Vampire.collection.insertMany(vampireData,() => {
+//     console.log(vampireData)
+//     mongoose.connection.close();
+//   });
+
+
 // ### Add some new vampire data
-Vampire.create(
-    {
-        name: "Nosferatu", 
-        hair_color: 'brown',
-        eye_color: 'blue',
-        dob: new Date(1088, 4, 4, 9, 8),
-        loves: ['hunting', 'killing people', 'night swims'],
-        location: 'Miami, FL, US',
-        gender: 'm',
-        title: 'The First Vampire'
-    },
-    (err, createdVampire) => {
-        if(err){
-            console.log(err);
-        } else {
-         console.log(createdVampire);
-        }
-    });
 
 
-Vampire.create(
-    {
-        name: "Vlad Transylvannia", 
-        hair_color: 'black',
-        eye_color: 'brown',
-        dob: new Date(1545, 5, 4, 11, 10),
-        loves: ['being merry', 'being insane', 'card games'],
-        location: 'Transylvania, Romania, EU',
-        gender: 'm',
-        title: 'Vlad the Impaler'
-    },
-    (err, createdVampire) => {
-        if(err){
-            console.log(err);
-        } else {
-         console.log(createdVampire);
-        }
-    });
+// Vampire.create(
+//     {
+//         name: "Nosferatu", 
+//         hair_color: 'brown',
+//         eye_color: 'blue',
+//         dob: new Date(1088, 4, 4, 9, 8),
+//         loves: ['hunting', 'killing people', 'night swims'],
+//         location: 'Miami, FL, US',
+//         gender: 'm',
+//         title: 'The First Vampire'
+//     },
+//     (err, createdVampire) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//          console.log(createdVampire);
+//         }
+//     });
 
-Vampire.create(
-    {
-        name: "Drucilla", 
-        hair_color: 'red',
-        eye_color: 'green',
-        dob: new Date(1784, 6, 3, 13, 12),
-        loves: ['being a maneater', 'big parties', 'card games'],
-        location: 'Paris, France, EU',
-        gender: 'f',
-        title: 'The Red Terror'
-    },
-    (err, createdVampire) => {
-        if(err){
-            console.log(err);
-        } else {
-         console.log(createdVampire);
-        }
-    });
 
-Vampire.create(
-    {
-        name: "Elvira", 
-        hair_color: 'brown',
-        eye_color: 'brown',
-        dob: new Date(1884, 6, 3, 13, 12),
-        loves: ['being merry', 'being insane', 'card games'],
-        location: 'Baton Rouge, LA, US',
-        gender: 'f',
-        title: 'The Creole Menace'
-    },
-    (err, createdVampire) => {
-    if(err){
-        console.log(err);
-    } else {
-     console.log(createdVampire);
-    }
-});
+// Vampire.create(
+//     {
+//         name: "Vlad Transylvannia", 
+//         hair_color: 'black',
+//         eye_color: 'brown',
+//         dob: new Date(1545, 5, 4, 11, 10),
+//         loves: ['being merry', 'being insane', 'card games'],
+//         location: 'Transylvania, Romania, EU',
+//         gender: 'm',
+//         title: 'Vlad the Impaler'
+//     },
+//     (err, createdVampire) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//          console.log(createdVampire);
+//         }
+//     });
+
+// Vampire.create(
+//     {
+//         name: "Drucilla", 
+//         hair_color: 'red',
+//         eye_color: 'green',
+//         dob: new Date(1784, 6, 3, 13, 12),
+//         loves: ['being a maneater', 'big parties', 'card games'],
+//         location: 'Paris, France, EU',
+//         gender: 'f',
+//         title: 'The Red Terror'
+//     },
+//     (err, createdVampire) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//          console.log(createdVampire);
+//         }
+//     });
+
+// Vampire.create(
+//     {
+//         name: "Elvira", 
+//         hair_color: 'brown',
+//         eye_color: 'brown',
+//         dob: new Date(1884, 6, 3, 13, 12),
+//         loves: ['being merry', 'being insane', 'card games'],
+//         location: 'Baton Rouge, LA, US',
+//         gender: 'f',
+//         title: 'The Creole Menace'
+//     },
+//     (err, createdVampire) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//      console.log(createdVampire);
+//     }
+// });
+
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
 
+// Vampire.find({gender: "f"}, (err, foundVampire) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//      console.log(foundVampire);
+//     }
+// });
+
+// Vampire.find({victims: {$gt: 500}}, (err, foundVampire) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//      console.log(foundVampire);
+//     }
+// });
+
+// Vampire.find({victims: {$lte: 150}}, (err, foundVampire) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//      console.log(foundVampire);
+//     }
+// });
+
+// Vampire.find({victims: {$ne: 210234}}, (err, foundVampire) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//      console.log(foundVampire);
+//     }
+// });
+
+
+Vampire.find({victims: {$gt: 150} && {$lt: 500}}, (err, foundVampire) => {
+    if(err){
+        console.log(err);
+    } else {
+     console.log(foundVampire);
+    }
+});
+
+
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
+
+
 
 /////////////////////////////////////////////////
 // ### Select with OR
